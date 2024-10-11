e42fa7522042650aa7d210b71b2bbf3393ab344e
Furqan Nadir <74790878+Furqan-Nadir@users.noreply.github.com>
2024-10-11 20:20:00 +0500

    Update ExponentialPlot.py

--- ExponentialPlot.py
+++ ExponentialPlot.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-name = input("Apna naam daalein: ")
-x = np.arange(-3, 3, 0.002)
+x = np.arange(-3, 3, 0.2)
 y = np.exp(x)
 plt.plot(x, y, color="g")
 plt.show()
